2911bd527e401f70ba038211c27500ac3df857f2
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-06-09 18:00:17 -0400

    Added pages

---
+++ docs/precache-manifest.e23a3dfd3d5309bee9eecac5b9fa1b59.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0295837a3d1c6c5e487560e72e859da5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df71cac94e5bed22b53d",
+    "url": "/static/css/main.2b1bcc7a.chunk.css"
+  },
+  {
+    "revision": "225012856469fa11834b",
+    "url": "/static/js/2.8e92883d.chunk.js"
+  },
+  {
+    "revision": "df71cac94e5bed22b53d",
+    "url": "/static/js/main.18c102ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3767d72785045cd4078b250c95e95037",
+    "url": "/static/media/3d space igloo.3767d727.svg"
+  },
+  {
+    "revision": "1082082896d07f7d86b4fdf5b40edf36",
+    "url": "/static/media/HomepageMain.10820828.svg"
+  },
+  {
+    "revision": "c443152b0dcea12176d0d9b544f267cd",
+    "url": "/static/media/OurSpaceIgloos.c443152b.svg"
+  },
+  {
+    "revision": "ed75156ba393cc33ee1124b097939b07",
+    "url": "/static/media/SorsLogo.ed75156b.svg"
+  },
+  {
+    "revision": "efc248db2f2e230e2c7d014858d52a38",
+    "url": "/static/media/blue.efc248db.svg"
+  },
+  {
+    "revision": "da51c33952348feaa383783ed604527a",
+    "url": "/static/media/book.da51c339.svg"
+  },
+  {
+    "revision": "7bd355d9d55eba27908504d43acf0444",
+    "url": "/static/media/bottom.7bd355d9.svg"
+  },
+  {
+    "revision": "c602f5b185331319f0e20c87ee9a2290",
+    "url": "/static/media/card.c602f5b1.svg"
+  },
+  {
+    "revision": "a0029e6a3c9b0bf9576ffde9428ba7c6",
+    "url": "/static/media/comfortable.a0029e6a.svg"
+  },
+  {
+    "revision": "f575172a159dc53f9364c2408f507d2c",
+    "url": "/static/media/compact.f575172a.svg"
+  },
+  {
+    "revision": "f0c8fd8c9f981bb5c56e71830ddb9a7e",
+    "url": "/static/media/green.f0c8fd8c.svg"
+  },
+  {
+    "revision": "e2f48a699b0a0b2a92a5fe5e16b7c47e",
+    "url": "/static/media/illust1.e2f48a69.svg"
+  },
+  {
+    "revision": "f1e1c2e2e37f636b9a556b7f013a108e",
+    "url": "/static/media/illust2.f1e1c2e2.svg"
+  },
+  {
+    "revision": "d8eb6e3ddcc514fa549e0e59117d12f6",
+    "url": "/static/media/illust3.d8eb6e3d.svg"
+  },
+  {
+    "revision": "2170a0f236d637bb1a0f0a04ab706eec",
+    "url": "/static/media/illust4.2170a0f2.svg"
+  },
+  {
+    "revision": "dae6fbbb25a4d5515ce1e2133ee92f0a",
+    "url": "/static/media/orange.dae6fbbb.svg"
+  },
+  {
+    "revision": "7450c5710a841cda9275c2978c441ba9",
+    "url": "/static/media/safe.7450c571.svg"
+  },
+  {
+    "revision": "45bbcae5236852a2cdb44f90a9658a33",
+    "url": "/static/media/top.45bbcae5.svg"
+  }
+]);
